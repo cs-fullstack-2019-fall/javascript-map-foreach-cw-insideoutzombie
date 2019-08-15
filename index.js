b01dd7ab84bc056adeaafa507a81c0b828567f64
insideoutzombie <david@web3devs.com>
2019-08-15 12:57:00 -0500

    attempted it

---
+++ index.js
@@ -0,0 +1,33 @@
+// ### Problem 1
+// Ask the user to enter a word. Keep asking them to enter a word until they enter the letter 'q'.
+let user;
+  while (user !== 'q') {
+    user = prompt("enter a word or hit 'q'to quit");
+    function userArray(arrayOfWords) {
+      var words = '';
+      // arrayOfWords.forEach(functionI(eachElement) {
+      //   words = user + eachElement;
+      // }
+    // );
+    }
+  }
+
+
+
+
+// ### Problem 2
+// Make any string array all lower case strings. If you don't remember how to use that Google it.
+// var array3 = ["Test", "It", "Slow"];
+// let asString = Arrays.toString(array3);
+// if(asString.equals(asString.toLowerCase())
+
+// ### Challenge
+// Make any string array return an array of objects with the specified class below
+//
+// ```
+// class Student{
+//         constructor(name){
+//                 this.name = name;
+//         }
+// }
+// ```
